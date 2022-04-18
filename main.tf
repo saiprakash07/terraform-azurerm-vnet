0af9b3a27ef5ddb9ecd398054f08656ac7ff2b3c
saiprakash07 <88754277+saiprakash07@users.noreply.github.com>
2022-04-18 14:31:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet1"
-  resource_group_name  = "rg1"
-  virtual_network_name = "vnet1"
-  address_prefixes     = ["10.0.1.0/24"]
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet1"
+  location            = "west europe"
+  resource_group_name = rg1
+  address_space       = ["10.0.0.0/16"]
   
+  tags = {
+    environment = "Production"
   }
+}
