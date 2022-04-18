adfa6124792623d4636fa07e57b6ba62caf7c6fc
saiprakash07 <88754277+saiprakash07@users.noreply.github.com>
2022-04-18 15:08:00 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet1"
   location            = "west europe"
-  resource_group_name = rg1
+  resource_group_name = "rg1"
   address_space       = ["10.0.0.0/16"]
   
   tags = {
