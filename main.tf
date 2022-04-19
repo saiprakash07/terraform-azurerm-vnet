60bd665bfa28cee2fefea02b900516ac03b72db0
sai <saitinku1996@gmail.com>
2022-04-19 14:17:16 -0400

    u
    p
    
    d
    
    a
    
    t
    
    e
    
    d
    
    a
    
    u
    
    a
    
    a

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = "rg1"
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
-  
+
   tags = {
     environment = "Production"
   }
